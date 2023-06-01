05a3d276641225eafc4ae7f386e6d8b3610288b4
NguyenTrongQuan <112452601+hapo-quannt@users.noreply.github.com>
2023-06-02 00:37:20 +0700

    refactor code

--- load.py
+++ load.py
@@ -4,9 +4,3 @@ import aspose.words as aw
 def convert_docx_to_html_file(file_path, output_file):
     doc = aw.Document(file_path)
     doc.save(output_file)
-
-docx_file = "E:\dowload\Test_Docx.docx"
-output_file = 'output.html'
-current_directory = os.getcwd()
-output_path = os.path.join(current_directory, output_file)
-convert_docx_to_html_file(docx_file, output_path)
